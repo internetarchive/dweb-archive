f9b1516c4f712a86cf29e78dd23a044fec6b1b12
Mitra Ardron <mitra@mitra.biz>
2019-06-28 18:45:29 +1000

    * v0.1.79: componentize: ItemDetailsAboutJSX, DownloadDirectory, SearchSwitcher, ImageDweb; NavNavWrapper; refactor members->membersFav,membersSearch; reload/no-cache on searches; obsolete nav_home;

--- Home.js
+++ Home.js
@@ -12,6 +12,4 @@ export default class Home extends Search {
             <center style={{margin: "35px"}}><span style={{fontSize: "125px"}} className="iconochive-logo"></span></center>
         );
     }
-    _fetch_metadata(opts, cb) { cb(null, this); } // No metadata for home
-    //TODO this can use the _fetch_metadata exception being coded 2019-06-06
 }
