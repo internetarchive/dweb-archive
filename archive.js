d5fc95c5251d5b883eb80b1c5dcf4d07dabb611d
Mitra Ardron <mitra@mitra.biz>
2019-09-29 16:42:01 +1000

    Move dweb-transports into bundle, and add dweb-archive-dist package.json for separate npm distro to reduce size of dweb-mirror

--- archive.js
+++ archive.js
@@ -1,5 +1,6 @@
 var Nav = require('./Nav').default;
 var gatewayServer = require('@internetarchive/dweb-archivecontroller/Util')["gatewayServer"]; // Needed by archive.html
 const { getImageURI } = require('./ReactSupport')
+var DwebTransports = require('@internetarchive/dweb-transports'); //Also sets window.DwebTransports
 window.DwebArchive = { Nav, gatewayServer, getImageURI }; // gatewayServer is for archive.html
 window.Nav = Nav; // So HTML can find it
