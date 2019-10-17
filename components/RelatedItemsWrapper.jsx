27bb2b011330a354f0794a8ed2ab8d636f64a505
Mitra Ardron <mitra@mitra.biz>
2019-10-17 14:06:21 +1100

    RelatedItems - code review and remove dependence on IAReactComponent

--- components/RelatedItemsWrapper.jsx
+++ components/RelatedItemsWrapper.jsx
@@ -1,10 +1,9 @@
-//const debug = require('debug')('ia-components:RelatedItemsWrapper');
 import React from 'react';
-import {IAReactComponent, RelatedItems} from '@internetarchive/ia-components/dweb-index.js';
+import {RelatedItems} from '@internetarchive/ia-components/dweb-index.js';
 
 //Note this component is a bridge from ArchiveBase.js to a IAUX React Component (that wants not to be dependent on archivecontroller,
 
-export default class RelatedItemsWrapper extends IAReactComponent {
+export default class RelatedItemsWrapper extends React.Component {
   /**
    * <RelatedItemsWrapper
    *    identifier: IDENTIFIER
@@ -20,11 +19,10 @@ export default class RelatedItemsWrapper extends IAReactComponent {
   constructor(props) {
     console.assert(props.item || props.members,"Must pass either item or members")
     super(props); //identifier, members, item
-    //TODO-STATE this might have the issue of constructor not being re-run and needing componentDidMount catch
-    this.state.members = this.props.members; // Maybe undefined, but has to be in .state so can change
+    this.state = {member: this.props.members}; // Maybe undefined, but has to be in .state so can change
     // called via ref=this.load when the component is loaded, triggers async load via API if .members undefined
     if (this.props.identifier && this.props.item && !this.state.members) {
-      this.setState({loading: true});
+      this.state = {loading: true};
       this.props.item.relatedItems({wantStream:false, wantMembers:true, noCache: this.props.noCache}, (err, members) => {
         if (!err) { // If there is an error then relatedItems() will have reported it, and can just ignore it here and not display
           this.setState({members: members, loading: false});
@@ -32,12 +30,31 @@ export default class RelatedItemsWrapper extends IAReactComponent {
       });
     }
   }
-
+  fetchRelated(item) {
+    this.setState({loading: true});
+    item.relatedItems({wantStream:false, wantMembers:true, noCache: this.props.noCache}, (err, members) => {
+      if (!err && (item === this.props.item)) {
+        // If there is an error then relatedItems() will have reported it, and can just ignore it here and not display
+        // Dont update if item has changed while relatedItems running
+        this.setState({members: members, loading: false});
+      }
+    });
+  }
+  componentDidMount() {
+    if (this.props.identifier && this.props.item && !this.state.members) {
+      this.fetchRelated(this.props.item);
+    }
+  }
+  componentDidUpdate(prevProps, unusedPrevState, unusedSnapshot) {
+    if (this.props.item && (this.props.item !== prevProps.item) ) { // Item changed refetch
+      this.fetchRelated(this.props.item);
+    }
+  }
   render() {
-    return (
+    return ((this.state.loading) ? null :
       // Static or asynchronously loaded members handled here
       <RelatedItems identifier={this.props.identifier} members={this.state.members} disconnected={this.props.disconnected} />
     );
   }
 }
-
+// Code review Mitra 2019-10-17
