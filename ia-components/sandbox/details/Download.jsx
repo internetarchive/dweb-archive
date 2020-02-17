c3328ff89b9ec9b27a5fbf37ace0784934dd1ed7
Mitra Ardron <mitra@mitra.biz>
2020-02-18 04:47:45 +1100

    Download remove IAReactComponent , linting

--- ia-components/sandbox/details/Download.jsx
+++ ia-components/sandbox/details/Download.jsx
@@ -1,11 +1,11 @@
 // const debug = require('debug')('dweb-archive:Download');
+/* eslint-disable react/prefer-stateless-function, react/prop-types */
 import React from 'react';
-import IAReactComponent from '../IAReactComponent';
 import AnchorDetails from '../AnchorDetails';
 import { AnchorDownload, reachable } from './AnchorDownload';
-import { I18nSpan, I18nStr } from "../languages/Languages";
+import { I18nSpan, I18nStr } from '../languages/Languages';
 
-class DownloadDirectoryDiv extends IAReactComponent {
+class DownloadDirectoryDiv extends React.Component {
   /**
    * <Download
    *    identifier: identifier of collection, undefined for searches
@@ -17,47 +17,75 @@ class DownloadDirectoryDiv extends IAReactComponent {
    * if downloaded=false then only displays files in list
    * TODO needs date in form probably like new Date().toLocaleDateString('en-GB',{day:'numeric', month:'short', year:'numeric',hour:'2-digit',minute:'2-digit'})
    * See https://github.com/internetarchive/dweb-archive/issues/10 for discussion - this is NOT complete yet, but works enough to use.
-   * https://github.com/internetarchive/dweb-archivecontroller/issues/8 for better algirithm for reachable, and probably apply at ArchiveItem level when getting list
+   * https://github.com/internetarchive/dweb-archivecontroller/issues/8 for better algorithm for reachable, and probably apply at ArchiveItem level when getting list
    */
 
   render() {
     return (
       <div className="container container-ia">
         <div className="download-directory-listing">
-          <h1><I18nSpan en="Files for"/> {this.props.identifier}</h1>
-          <hr/>
+          <h1>
+            <I18nSpan en="Files for" />
+            {' '}
+            {this.props.identifier}
+          </h1>
+          <hr />
           <pre>
             <table className="directory-listing-table">
-              <thead><tr><th><I18nSpan en="Name"/></th>
-              {/*--<th>Last modified</th>--*/}
-              <th>Size</th></tr></thead>
+              <thead>
+                <tr>
+                  <th><I18nSpan en="Name" /></th>
+                  {/* --<th>Last modified</th>--*/}
+                  <th>Size</th>
+                </tr>
+              </thead>
               <tbody>
                 <tr>
-                  <td><AnchorDetails identifier={this.props.identifier}><span className="iconochive-Uplevel"
-                                                                              title={I18nStr("Parent Directory")}
-                                                                              aria-hidden="true"></span> <I18nSpan en="Go to item page"/></AnchorDetails></td>
-                  <td></td>
-                  <td></td>
+                  <td>
+                    <AnchorDetails identifier={this.props.identifier}>
+                      <span
+                        className="iconochive-Uplevel"
+                        title={I18nStr('Parent Directory')}
+                        aria-hidden="true"
+                      />
+                      {' '}
+                      <I18nSpan en="Go to item page" />
+                    </AnchorDetails>
+                  </td>
+                  <td />
+                  <td />
                 </tr>
                 {this.props.files
-                  .filter(f => reachable({disconnected: this.props.disconnected, source: f}))
+                  .filter(f => reachable({ disconnected: this.props.disconnected, source: f }))
                   .map(f => (
                     <tr key={f.name}>
-                      <td><AnchorDownload source={f}
-                                          title={f.name}
-                                          disconnected={this.props.disconnected}><I18nSpan
-                        className="sr-only" en="download"/>{f.name}</AnchorDownload></td>
-                      {/*--TODO-DIR handle directory here as foo/ --*/}
-                      {/*--<td>00-Apr-0000 00:00{--TODO-DIR handle date from mtime </td>--*/}
-                      <td>{f.size}{/*--TODO-DIR should be "-" for directory--*/}</td>
+                      <td>
+                        <AnchorDownload
+                          source={f}
+                          title={f.name}
+                          disconnected={this.props.disconnected}
+                        >
+                          <I18nSpan
+                            className="sr-only"
+                            en="download"
+                          />
+                          {f.name}
+                        </AnchorDownload>
+                      </td>
+                      {/* --TODO-DIR handle directory here as foo/ --*/}
+                      {/* --<td>00-Apr-0000 00:00{--TODO-DIR handle date from mtime </td>--*/}
+                      <td>
+                        {f.size}
+                        {/* --TODO-DIR should be "-" for directory--*/}
+                      </td>
                     </tr>
-                ))}
+                  ))}
               </tbody>
             </table>
           </pre>
         </div>
       </div>
-    )
+    );
   }
 }
-export {DownloadDirectoryDiv}
+export { DownloadDirectoryDiv };
