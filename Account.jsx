a7fd26e86be23a599519e82da0a5e1123fd4c154
Mitra Ardron <mitra@mitra.biz>
2020-02-17 02:49:14 -0800

    Account - remove IAReactComponent

--- Account.jsx
+++ Account.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
-import {SearchRowColumnsItems} from "./components/SearchPage";
-import {IAReactComponent, ImageDweb, Tabby, NavWrap, I18nSpan, I18nIcon} from './ia-components/dweb-index';
+import { SearchRowColumnsItems } from './components/SearchPage';
+import { ImageDweb, Tabby, NavWrap, I18nSpan, I18nIcon } from './ia-components/dweb-index';
 
-class AccountTabbyPosts extends IAReactComponent {
-    /**
+class AccountTabbyPosts extends React.Component {
+  /**
      * <AccountTabbyPosts item=ARCHIVEITEM />
      * TODO pass in detailed props rather than item
      */
-    render() {
-        let name = this.props.item.itemid.replace(/_/g, ' ').replace('@', ''); // Canonical name from id, may have better ways to get this - see @tracey_pooh for case needing this one.
-        return (
-          <div className="container container-ia nopad">
-              <div id="tabby-posts" className="tabby-data hidden row">
-                  <div className="box">
-                      <h1>Posts by {name}</h1>
-                      {/*-- NOTE: extra div required by IE bec. table width=100% is set --*/}
-                      <div>
-                          <table className="forumTable  table table-striped table-condensed table-hover">
-                            <tbody>
-                              <tr className="backColor1 forumRowHead">
-                                  <td><I18nSpan en="Subject"/></td>
-                                  <td><I18nSpan en="Poster"/></td>
-                                  <td><I18nSpan en="Forum"/></td>
-                                  <td><I18nSpan en="Replies"/></td>
-                                  <td><I18nSpan en="Date"/></td>
-                              </tr>
-                              {/*-- TODO-ACCOUNT this is a loop  need query for forum posts https://github.com/internetarchive/dweb-archive/issues/86--*/}
-                              <tr>
-                                  <td><Ispan en="Still being implemented"/></td>
-                              </tr>
-                              {/*
+  render() {
+    const name = this.props.item.itemid.replace(/_/g, ' ').replace('@', ''); // Canonical name from id, may have better ways to get this - see @tracey_pooh for case needing this one.
+    return (
+      <div className="container container-ia nopad">
+        <div id="tabby-posts" className="tabby-data hidden row">
+          <div className="box">
+            <h1>
+Posts by
+              {name}
+            </h1>
+            {/* -- NOTE: extra div required by IE bec. table width=100% is set --*/}
+            <div>
+              <table className="forumTable  table table-striped table-condensed table-hover">
+                <tbody>
+                  <tr className="backColor1 forumRowHead">
+                    <td><I18nSpan en="Subject" /></td>
+                    <td><I18nSpan en="Poster" /></td>
+                    <td><I18nSpan en="Forum" /></td>
+                    <td><I18nSpan en="Replies" /></td>
+                    <td><I18nSpan en="Date" /></td>
+                  </tr>
+                  {/* -- TODO-ACCOUNT this is a loop  need query for forum posts https://github.com/internetarchive/dweb-archive/issues/86--*/}
+                  <tr>
+                    <td><Ispan en="Still being implemented" /></td>
+                  </tr>
+                  {/*
                                 <tr valign="top" className="eve forumRow">
                                     <td>
                                         <a href="/post/1089612">Re: Why can't we adjust the volume anymore?</a>
@@ -77,125 +80,132 @@ class AccountTabbyPosts extends IAReactComponent {
                                     </td>
                                 </tr>
                                 */}
-                              {/* -- End of forum loop */}
-                            </tbody>
-                          </table>
-                          {/* TODO-ACCOUNT rethink when have query above
+                  {/* -- End of forum loop */}
+                </tbody>
+              </table>
+              {/* TODO-ACCOUNT rethink when have query above
                             <br/><b><a href="/iathreads/forum-display.php?poster=tracey%20pooh&limit=100">View more forum posts</a></b>
                             */}
-                      </div>
-                  </div>
-              </div>
-              {/*--/#.tabby-posts--*/}
+            </div>
           </div>
-        )
-    }
+        </div>
+        {/* --/#.tabby-posts--*/}
+      </div>
+    );
+  }
 }
 class AccountWelcome extends IAReactComponent {
-    /**
+  /**
      * <AccountWelcome item=ARCHIVEITEM />
      * TODO pass in detailed props rather than item
      * - see similar style/HTML in CommonWelcome CollectionBanner and AccountBanner
      */
-    render() {
-
-        let identifier = this.props.item.itemid;
-        let title = this.props.item.metadata.title; // Example from @tracey_pooh
-        let imagesrc = this.props.item.thumbnailFile() || "/images/notfound.png" ;  // TODO-ACCOUNT Tracey is adding a service to get a better image (larger scale
-        let name = this.props.item.itemid.replace(/_/g, ' ').replace('@', ''); // Canonical name from id, may have better ways to get this - see @tracey_pooh for case needing this one.
-        return (
-            <div className="welcome container container-ia width-max account-uniform">
-                <div className="container">
-                    <div className="row">
-                                <div className="col-xs-11 col-sm-2 welcome-left" style={{textAlign: "center"}}>
-                                    <div id="file-dropper-wrap">
-                                        <div id="file-dropper"></div>
-                                        <ImageDweb id="file-dropper-img" source={imagesrc} style={{maxWidth:"300px"}}/>
-                                    </div>
-                                </div>{/*--/.col-xs-11--*/}
-                                <div className="col-xs-1 col-sm-2 col-sm-push-8 welcome-right">
-                                    {/* probably ok as absolute link since requires ID anyway */}
-                                    {/* Removed onclick="return AJS.modal_go(this.props.item,{favorite:1})"  - not compatible with /bookmarks rewrite */}
-                                    <a className="stealth"
-                                       href={`/bookmarks.php?add_bookmark=1&amp;title=people&amp;mediatype=account&amp;identifier=${identifier}`}
-                                       data-target="#confirm-modal"><I18nIcon
-                                            className="iconochive-favorite" en="favorite" xs="Favorite"/></a><br/>
-                                </div>
-                                <div className="clearfix visible-xs-block hidden-sm hidden-md hidden-lg"></div>
-                                <div className="col-xs-12 col-sm-8 col-sm-pull-2">
-                                    <h1>{name}</h1>
-                                    <h5 className="account-description">{title}</h5>
-                                </div>
-                    </div>{/*--/.row--*/}
-                    <div className="tabbys tabbys-dynamic">
-                        <Tabby identifier={identifier} id="uploads" text="UPLOADS" default/>
-                        {/*<Tabby identifier={identifier} id="posts" text="POSTS"/>*/}
-                        {/*<Tabby identifier={identifier} id="reviews" text="REVIEWS"/>*/}
-                        {/*<Tabby identifier={identifier} id="collections" text="COLLECTIONS"/>*/}
-                        {/*<Tabby identifier={identifier} id="loans" text="LOANS"/>
+  render() {
+    const identifier = this.props.item.itemid;
+    const title = this.props.item.metadata.title; // Example from @tracey_pooh
+    const imagesrc = this.props.item.thumbnailFile() || '/images/notfound.png'; // TODO-ACCOUNT Tracey is adding a service to get a better image (larger scale
+    const name = this.props.item.itemid.replace(/_/g, ' ').replace('@', ''); // Canonical name from id, may have better ways to get this - see @tracey_pooh for case needing this one.
+    return (
+      <div className="welcome container container-ia width-max account-uniform">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-11 col-sm-2 welcome-left" style={{ textAlign: 'center' }}>
+              <div id="file-dropper-wrap">
+                <div id="file-dropper" />
+                <ImageDweb id="file-dropper-img" source={imagesrc} style={{ maxWidth: '300px' }} />
+              </div>
+            </div>
+            {/* --/.col-xs-11--*/}
+            <div className="col-xs-1 col-sm-2 col-sm-push-8 welcome-right">
+              {/* probably ok as absolute link since requires ID anyway */}
+              {/* Removed onclick="return AJS.modal_go(this.props.item,{favorite:1})"  - not compatible with /bookmarks rewrite */}
+              <a className="stealth"
+                href={`/bookmarks.php?add_bookmark=1&amp;title=people&amp;mediatype=account&amp;identifier=${identifier}`}
+                data-target="#confirm-modal"
+              >
+                <I18nIcon
+                  className="iconochive-favorite" en="favorite" xs="Favorite"
+                />
+              </a>
+              <br />
+            </div>
+            <div className="clearfix visible-xs-block hidden-sm hidden-md hidden-lg" />
+            <div className="col-xs-12 col-sm-8 col-sm-pull-2">
+              <h1>{name}</h1>
+              <h5 className="account-description">{title}</h5>
+            </div>
+          </div>
+          {/* --/.row--*/}
+          <div className="tabbys tabbys-dynamic">
+            <Tabby identifier={identifier} id="uploads" text="UPLOADS" default />
+            {/* <Tabby identifier={identifier} id="posts" text="POSTS"/> */}
+            {/* <Tabby identifier={identifier} id="reviews" text="REVIEWS"/> */}
+            {/* <Tabby identifier={identifier} id="collections" text="COLLECTIONS"/> */}
+            {/* <Tabby identifier={identifier} id="loans" text="LOANS"/>
                         {/*<Tabby identifier={identifier} id="web-archives" text="WEB ARCHIVES" abbreviatedText="WEB"
-                               href={`/details/${identifier}/web-archive`}/>*/}
-                        <div className="clearfix"></div>
-                    </div>
-                </div>{/*--/.container--*/}
-            {/*--/.welcome--*/}</div>
-        );
-    }
-
+                               href={`/details/${identifier}/web-archive`}/> */}
+            <div className="clearfix" />
+          </div>
+        </div>
+        {/* --/.container--*/}
+        {/* --/.welcome--*/}
+      </div>
+    );
+  }
 }
 
 class AccountUploads extends IAReactComponent {
-    /**
+  /**
      * <AccountUploads item=ARCHIVEITEM disconnected=BOOL />
      */
-    render() {
-        return (
-          <div className="container container-ia nopad">
-              <div id="tabby-uploads" className="tabby-data in">
-                  <SearchRowColumnsItems item={this.props.item} disconnected={this.props.disconnected} />
-              </div>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container container-ia nopad">
+        <div id="tabby-uploads" className="tabby-data in">
+          <SearchRowColumnsItems item={this.props.item} disconnected={this.props.disconnected} />
+        </div>
+      </div>
+    );
+  }
 }
 
 class AccountWrap extends IAReactComponent {
-    /**
+  /**
      * <AccountWrap item=ARCHIVEITEM/>
      * @returns {*}
      */
-    render() {
-        /*
+  render() {
+    /*
         Wraps all page content
         Context: body wrap script
         Content: wrap ( navwrap, main ( script, account-uniform ( description&picture, tabbys-dynamic), tabby-uploads ( facets, columns-items ) ) )
 
         Built out from a download of Tracey's account page on 10June2018
          */
-        //if (typeof AJS != 'undefined') AJS.lists_v_tiles_setup('account');
-        {/* Missing donate-banner and scripts & css before it */}
-        return (
-          <>
-            <NavWrap
-              item={this.props.item}
-              transportStatuses={this.props.transportStatuses}
-              transportsClickable={this.props.transportsClickable}
-              mirror2gateway={this.props.mirror2gateway}
-              disconnected={this.props.disconnected}
-              canSave={true}
-            />
-            {/*--Begin page content --*/}
-            <main id="maincontent">
-                <div className="container container-ia">
-                    {/*-- <script> if (typeof AJS != 'undefined') AJS.lists_v_tiles_setup('account'); </script> --*/}
-                </div>
-                {/*--.container-ia --*/}
-                <AccountWelcome item={this.props.item}/>
-                <AccountUploads item={this.props.item} disconnected={this.props.disconnected}/>
-              {/*<AccountTabbyPosts item={this.props.item}/>*/}
-            </main>
-          </>
-        );
-    }
+    // if (typeof AJS != 'undefined') AJS.lists_v_tiles_setup('account');
+    { /* Missing donate-banner and scripts & css before it */ }
+    return (
+      <>
+        <NavWrap
+          item={this.props.item}
+          transportStatuses={this.props.transportStatuses}
+          transportsClickable={this.props.transportsClickable}
+          mirror2gateway={this.props.mirror2gateway}
+          disconnected={this.props.disconnected}
+          canSave
+        />
+        {/* --Begin page content --*/}
+        <main id="maincontent">
+          <div className="container container-ia">
+            {/* -- <script> if (typeof AJS != 'undefined') AJS.lists_v_tiles_setup('account'); </script> --*/}
+          </div>
+          {/* --.container-ia --*/}
+          <AccountWelcome item={this.props.item} />
+          <AccountUploads item={this.props.item} disconnected={this.props.disconnected} />
+          {/* <AccountTabbyPosts item={this.props.item}/> */}
+        </main>
+      </>
+    );
+  }
 }
-export {AccountWrap};
+export { AccountWrap };
